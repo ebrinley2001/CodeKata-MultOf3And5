583cae8635348a408897e9b0a07583d757b86b8c
ebrinley2001 <brinleyethan@gmail.com>
2020-06-22 20:46:18 -0400

    Plz Work Now

---
+++ MultOf3And5Prod/ProdCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultOf3And5Prod
+{
+    public class ProdCode
+    {   
+        public static int MultOf3And5(int loopVal)
+        {
+            var sumOfOdd = new int();
+
+         for (int i = 1; i <= loopVal; i+= 1)
+            {
+                if (i % 5 == 0 || i % 3 == 0)
+                {
+                    sumOfOdd += i;
+                }
+            }
+            return sumOfOdd;
+        }
+    }
+}
---
+++ MultOf3And5ProdTests/ProdCodeTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultOf3And5Prod.Tests
+{
+    [TestClass()]
+    public class ProdCodeTests
+    {
+        [TestMethod()]
+        public void MultOf3And5Test()
+        {
+            Assert.IsNotNull(ProdCode.MultOf3And5(1));
+        }
+
+        [DataTestMethod]
+
+        [DataRow(10, 33)]
+        [DataRow(15, 60)]
+        [DataRow(20, 98)]
+        [DataRow(30, 225)]
+        [DataRow(999, 233168)]
+        public void Value_Mult_Check(int a, int b)
+        {
+            Assert.AreEqual(b, ProdCode.MultOf3And5(a));
+        }
+    }
+}
